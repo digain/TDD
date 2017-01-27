07e7ce91c8a129d8c59ed097f4cebad9e482763e
Adam Raczynski <raq000@gmail.com>
2017-01-27 23:38:52 +0100

    Basic Dollar and Franc imp.

---
+++ TDD/TDD/Models/Dollar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Models
+{
+    public class Dollar
+    {
+        int Amount;
+        public Dollar(int amount)
+        {
+            Amount = amount;
+        }
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(Amount * multiplier);
+        }
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return Amount == dollar.Amount;
+        }
+    }
+}
---
+++ TDD/TDD/Models/Franc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Models
+{
+    public class Franc
+    {
+        int Amount;
+        public Franc(int amount)
+        {
+            Amount = amount;
+        }
+        public Franc Times(int multiplier)
+        {
+            return new Franc(Amount * multiplier);
+        }
+        public override bool Equals(object obj)
+        {
+            Franc dollar = (Franc)obj;
+            return Amount == dollar.Amount;
+        }
+    }
+}
